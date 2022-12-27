eae547711ba339dc4691e041e16ac6654a11009e
Zachary Soja <zachary3003@gmail.com>
2022-12-27 17:21:23 +0100

    feature(frontend): update router

--- GymSite/gymsite-ui/src/router/router.js
+++ GymSite/gymsite-ui/src/router/router.js
@@ -1,56 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const path = (route, name, component) => ({ path: route, name, component: () => import(`@/views/${component}.vue`)})
+const path = (route, name, view) => ({ path: '/' + route, name, component: () => import(`@/views/${view}View.vue`)})
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: () => import('../views/HomeView.vue')
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: () => import('@/views/RegisterView.vue')
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('@/views/LoginView.vue')
-    },
-    {
-        path: '/user/:id',
-        name: 'user',
-        component: () => import('@/views/ProfileView.vue'),
-    },
-    {
-        path: '/user/update',
-        name: 'update-profile',
-        component: () => import('@/views/UpdateProfileView.vue')
-    },
-    {
-        path: '/admin',
-        name: 'admin',
-        component: () => import('@/views/AdminView.vue')
-    },
-    {
-        path: '/exercise',
-        name: 'exercise-list',
-        component: () => import("@/views/ExerciseListView.vue")
-    },
-    {
-        path: '/exercise/:id',
-        name: 'exercise',
-        component: () => import('@/views/ExerciseView.vue')
-    },
-    {
-        path: '/exercise/add',
-        name: 'add-exercise',
-        component: () => import('@/views/AddExerciseView.vue')
-    },
-    path('/workout', 'workout-list', 'WorkoutListView'),
-    path('/workout/add', 'add-workout', 'AddWorkoutView'),
-    path('/workout/:id', 'workout', 'WorkoutView'),
+    path('', 'home', 'Home'),
+    path('register', 'register', 'Register'),
+    path('login', 'login', 'Login'),
+    path('user/:id', 'user', 'Profile'),
+    path('user/update', 'update-user', 'UpdateProfile'),
+    path('admin', 'admin', 'Admin'),
+    path('exercise', 'exercise-list', 'ExerciseList'),
+    path('exercise/:id', 'exercise', 'Exercise'),
+    path('exercise/add', 'add-exercise', 'AddExercise'),
+    path('workout', 'workout-list', 'WorkoutList'),
+    path('workout/add', 'add-workout', 'AddWorkout'),
+    path('workout/:id', 'workout', 'Workout'),
 ]
 
 export default createRouter({
